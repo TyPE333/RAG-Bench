cf340799a4c3a745601056add443f26c57b3b0b3
TyPE333 <rohanavireddy90@gmail.com>
2025-04-28 15:54:32 -0700

    Implemented utilities for retrieval

---
+++ utils/tokenizer.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def simple_tokenize(text: str) -> List[str]:
+    """
+    Tokenize the input text by simple python split method."""
+
+    return text.split()
+
+### Add other tokenization methods here as needed ###
