6832bf95e19f69d5eef49c6db56b497316a729ad
TyPE333 <rohanavireddy90@gmail.com>
2025-04-29 14:39:15 -0700

    Fixed tokenizer to properly normalize (including case)

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -4,6 +4,6 @@ def simple_tokenize(text: str) -> List[str]:
     """
     Tokenize the input text by simple python split method."""
 
-    return text.split()
+    return text.lower().split()
 
 ### Add other tokenization methods here as needed ###
